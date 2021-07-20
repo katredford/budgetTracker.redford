baf74e211884169561bfb43a6c58598d4fda9136
katredford <katharine.redford@gmail.com>
2021-07-20 16:26:14 -0500

    add fetch in service worker

--- service-worker.js
+++ service-worker.js
@@ -46,3 +46,19 @@ self.addEventListener("activate", function (e) {
 		})
 	);
 });
+
+self.addEventListener("fetch", function (e) {
+	console.log("fetch request : " + e.request.url);
+	e.respondWith(
+		caches.match(e.request).then(function (request) {
+			if (request) {
+				console.log("responding with cache : " + e.request.url);
+				return request;
+			} else {
+				// if there are no cache, try fetching request
+				console.log("file is not cached, fetching : " + e.request.url);
+				return fetch(e.request);
+			}
+		})
+	);
+});
