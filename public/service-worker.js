521e86a1c86b480fc7c8be0e87904b96070bbf9b
katredford <katharine.redford@gmail.com>
2021-07-20 17:59:42 -0500

    works now

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,19 +3,7 @@ const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 self.addEventListener("install", function (e) {});
-const FILES_TO_CACHE = [
-	"./index.html",
-	"./events.html",
-	"./tickets.html",
-	"./schedule.html",
-	"./assets/css/style.css",
-	"./assets/css/bootstrap.css",
-	"./assets/css/tickets.css",
-	"./dist/app.bundle.js",
-	"./dist/events.bundle.js",
-	"./dist/tickets.bundle.js",
-	"./dist/schedule.bundle.js",
-];
+const FILES_TO_CACHE = ["./index.html", "./css/styles.css"];
 
 self.addEventListener("install", function (e) {
 	e.waitUntil(
